fc438503c554166ba2181656e981dbbac7db5f21
Raquel Benito <raquelbenitop@gmail.com>
2024-04-29 23:24:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+
+let usuarios = [
+    { id: 1, nombre: 'Ryu', edad: 32, lugarProcedencia: 'Japón' },
+    { id: 2, nombre: 'Chun-Li', edad: 29, lugarProcedencia: 'China' },
+    { id: 3, nombre: 'Guile', edad: 35, lugarProcedencia: 'Estados Unidos' },
+    { id: 4, nombre: 'Dhalsim', edad: 45, lugarProcedencia: 'India' },
+    { id: 5, nombre: 'Blanka', edad: 32, lugarProcedencia: 'Brasil' },
+]
+
+//READ
+app.get("/", (req, res) =>{
+  res.send(`
+    <h1>Lista de usuarios</h1>
+    <ul>
+      ${usuarios.map(usuario => `<li>ID: ${usuario.id} | Nombre: ${usuario.nombre} | Edad: ${usuario.edad} | Lugar de Procedencia: ${usuario.lugarProcedencia}</li>`).join("")}
+    </ul>
+    <form action="/usuarios" method="post">
+      <label for"nombre">Nombre</label>
+      <input type="text" id="nombre" name="nombre" required>
+
+      <label for"edad">Edad</label>
+      <input type="text" id="edad" name="edad" required>
+
+      <label for"lugarProcedencia">Lugar de Procedencia</label>
+      <input type="text" id="lugarProcedencia" name="lugarProcedencia" required>
+      <button type="submit">Agregar Usuario</button>
+    </form>
+    <a href="/usuarios">Usuarios JSON</a>
+  `)
+})
+
+//CREATE
+app.get("/usuarios", (req, res) => {
+  res.json(usuarios)
+})
+
+
+app.post("/usuarios", (req, res)=> {
+  const nuevoUsuario = {
+    id: usuarios.length + 1,
+    nombre: req.body.nombre,
+    edad: req.body.edad,
+    lugarProcedencia: req.body.lugarProcedencia
+  }
+  usuarios.push(nuevoUsuario)
+  res.redirect("/")
+})
+
+//Encontar por cada nombre de usuario
+app.get("/usuarios/:nombre", (req, res) => {
+    const nombreUser = req.params.nombre
+    const usuario = usuarios.find(user => user.nombre === nombreUser)
+    res.json(usuario)
+})
+
+
+
+app.listen(3000, ()=>{
+  console.log("Express está escuchando en el puerto 3000")
+})
